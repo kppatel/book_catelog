ff9520b5aedee2423487fe6e26be7137be71464a
PHP User <php_user@congenial-amd-1.(none)>
2013-01-02 12:33:38 +0530

    fixed star rating bug

--- application/views/books/edit.php
+++ application/views/books/edit.php
@@ -1,4 +1,4 @@
-<form action="" method="post" onload="checkStar('<?php echo $book['rating']; ?>')">
+		<form action="" method="post">
 			<fieldset>
 				<legend>Edit Book</legend>
 				<label for="name">Title</label>
@@ -19,14 +19,14 @@
 						'Read'  => 'Read',
 						'Unread'    => 'Unread'
 					), $book['reading_status']) ?>
-				<br
+				<br>
 
-					<label>Rating</label>
-				<input type="radio" name="rating" class="rating" value="1">
-				<input type="radio" name="rating" class="rating" value="2">
-				<input type="radio" name="rating" class="rating" value="3">
-				<input type="radio" name="rating" class="rating" value="4">
-				<input type="radio" name="rating" class="rating" value="5">
+				<label>Rating</label>
+				<input type="radio" name="rating" class="rating" value="1"<?php echo $book['rating'] == '1' ? ' checked' : '' ?>>
+				<input type="radio" name="rating" class="rating" value="2"<?php echo $book['rating'] == '2' ? ' checked' : '' ?>>
+				<input type="radio" name="rating" class="rating" value="3"<?php echo $book['rating'] == '3' ? ' checked="checked"' : '' ?>>
+				<input type="radio" name="rating" class="rating" value="4"<?php echo $book['rating'] == '4' ? ' checked' : '' ?>>
+				<input type="radio" name="rating" class="rating" value="5"<?php echo $book['rating'] == '5' ? ' checked' : '' ?>>
 				<?php echo form_error('rating') ?>
 				<br>
 
@@ -36,11 +36,10 @@
 				<?php echo anchor('books/index', 'Cancel') ?>
 			</fieldset>
 		</form>
-<?php echo css('rating'), js('jquery'), js('rating') ?>
-<script>
+
+		<?php echo css('rating'), js('jquery'), js('rating') ?>
+		<script>
 		jQuery(function($) {
 			$('input.rating').rating();
-			$('input').rating('select',2)
-
 		});
 		</script>
